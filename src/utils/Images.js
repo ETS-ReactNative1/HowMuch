d94eaddb619220786e611c994ffc686beb538a63
developer-appstirr <development@appstirr.com>
2022-04-21 16:26:19 +0500

    up

--- src/utils/Images.js
+++ src/utils/Images.js
@@ -3,6 +3,8 @@ const Images = {
     arrow: require('../assets/images/vector.png'),
     eyeicon: require('../assets/images/eyeicon.png'),
     eye: require('../assets/images/eye.png'),
+    dummy: require('../assets/images/dummy.png'),
+    orangePlus: require('../assets/images/orangePlus.png'),
     question: require('../assets/images/question.png')
   }
 }
